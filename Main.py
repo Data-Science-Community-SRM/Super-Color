4588f85e02806201cd7e5c775553fd00c9dad477
Aditya Shukla <adityashukzy@gmail.com>
2020-09-24 18:10:03 +0530

    Updated main.py and data_loader

--- Main.py
+++ Main.py
@@ -1,6 +1,36 @@
 import data_loader as dl
 import matplotlib.pyplot as plt
 
+
+def train(model, train_loader):
+    losses = []
+
+    for epoch in range(100):
+        running_loss = 0.0
+
+        for images, labels in train_loader:
+            # reinitializing gradients to zero to prevent exploding gradients
+            optimizer.zero_grad()
+
+            # making our predictions
+            output = model(images)
+
+            # calculating loss
+            loss = criterion(output, labels)
+
+            # backprop step
+            loss.backward()
+
+            running_loss += loss.item()
+            losses.append(running_loss)
+
+            optimizer.step()
+
+    fig = plt.figure()
+    plt.plot(losses)
+    plt.xlabel('Epochs')
+    plt.ylabel('Losses')
+
 def main():
     print("Running main now")
 
@@ -16,9 +46,15 @@ def main():
     # plt.show()
 
     # I think these two lines should probably print the first image
-    for img, _ in dataloader:
-        plt.imshow(img[0])
-        plt.show()
+    data_iter = iter(train_loader)
+    images, labels = next(data_iter)
+
+    for num in range(1, 6):
+        plt.subplot(10, 10, num)
+        plt.axis('off')
+        plt.imshow(images[num].numpy().squeeze())
+
+
 
     
 # This will call main function only when this is the main file
